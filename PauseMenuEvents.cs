730757c0a9fd321a150754da86a935cd58657049
Ashton Barber <33004309+bashtonarber@users.noreply.github.com>
2025-08-27 12:41:28 +0100

    Update PauseMenuEvents.cs

--- PauseMenuEvents.cs
+++ PauseMenuEvents.cs
@@ -45,42 +45,49 @@ After having a play around with it, the flow of it proved to be really useful an
     private bool musicToggled;
     private bool sfxToggled;
 
-//Awake function to trigger on launch.
+    //Awake function to trigger on launch.
     void Awake()
     {
-//Ensuring that the Bools are set to what's expected.
+        //Ensuring that the Bools are set to what's expected.
         gameIsPaused = false;
         settingsOpened = false;
         musicToggled = true;
         sfxToggled = true;
 
-//Getting the required components for this script
+        //Getting the required components for this script.
         _pauseDocument = GetComponent<UIDocument>();
         _musicSource = GetComponent<AudioSource>();
 
-//Declaring what each button is on the UIDocument
+        //Declaring what each button is within the UIDocument.
         _continueButton = _pauseDocument.rootVisualElement.Q("ContinueButton") as Button;
         _settingsButton = _pauseDocument.rootVisualElement.Q("SettingsButton") as Button;
         _mainMenuButton = _pauseDocument.rootVisualElement.Q("MainMenuButton") as Button;
         _closeGameButton = _pauseDocument.rootVisualElement.Q("CloseGameButton") as Button;
         _closeSettingsButton = _pauseDocument.rootVisualElement.Q("CloseSettingsButton") as Button;
 
+        //Declaring each VisualElement that will be used.
         _pauseBGElement = _pauseDocument.rootVisualElement.Q("PauseBG");
         _settingsBGElement = _pauseDocument.rootVisualElement.Q("SettingsMenu");
 
+        //Declaring the Toggles within the UIDocument.
         _musicToggle = _pauseDocument.rootVisualElement.Q("MusicToggle") as Toggle;
         _sfxToggle = _pauseDocument.rootVisualElement.Q("SFXToggle") as Toggle;
 
+        //Setting up each button to what function it will use. 
         _continueButton.RegisterCallback<ClickEvent>(ResumeGame);
         _settingsButton.RegisterCallback<ClickEvent>(SettingsMenuOpenClose);
         _mainMenuButton.RegisterCallback<ClickEvent>(ReturnToMM);
         _closeGameButton.RegisterCallback<ClickEvent>(CloseGame);
         _closeSettingsButton.RegisterCallback<ClickEvent>(SettingsMenuOpenClose);
 
+        //Setting up each Toggle to what function it will use.
         _musicToggle.RegisterCallback<ClickEvent>(ToggleMusic);
         _sfxToggle.RegisterCallback<ClickEvent>(ToggleSFX);
 
+        //Listing the buttons that are within in the UIDocument
         _pauseMenuButtons = _pauseDocument.rootVisualElement.Query<Button>().ToList();
+
+        // This counts the list of buttons and then calls the function that will trigger when any button is pressed within the UIDocument.
         for (int i = 0; i < _pauseMenuButtons.Count; i++)
         {
             _pauseMenuButtons[i].RegisterCallback<ClickEvent>(OnAllButtons);
@@ -214,4 +221,5 @@ After having a play around with it, the flow of it proved to be really useful an
         _musicSource.Play();
     }
 }
+
 
